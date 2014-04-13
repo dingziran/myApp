082f58507d2659442bf566af90679c47874bd88a
dingziran <dingziranrr@163.com>
2014-04-13 11:41:34 +0800

    add gitignore

---
+++ public/javascripts/controllers.js
@@ -0,0 +1,65 @@
+/**
+ * Created by Administrator on 14-3-17.
+ */
+var app=angular.module("myApp");
+app.controller('ListController',function($scope,$firebase,$location){
+    var usersRef = new Firebase("https://luminous-fire-4025.firebaseio.com/user");
+    $scope.users = $firebase(usersRef);
+    $scope.remove = function(user){
+        for(var item in $scope.users){
+            if($scope.users[item]==user){
+                $scope.users.$remove(item);
+                break;
+            }
+        }
+    }
+    $scope.update = function(user){
+        for(var item in $scope.users){
+            if($scope.users[item]==user){
+                $location.path("update/"+item);
+                break;
+            }
+        }
+    }
+});
+app.controller('CreateController',function($scope,$firebase){
+    var usersRef = new Firebase("https://luminous-fire-4025.firebaseio.com/user");
+    $scope.users = $firebase(usersRef);
+    $scope.user={};
+    $scope.create = function() {
+        // AngularFire $add method
+        $scope.users.$add($scope.user);
+        $scope.user = {};
+        $scope.message="create success!";
+    };
+});
+
+app.controller('LoginController',function($scope){
+    $scope.fields = [
+        {placeholder: 'Username', isRequired: true},
+        {placeholder: 'Password', isRequired: true},
+        {placeholder: 'Email (optional)', isRequired: false}
+    ];
+
+    $scope.submitForm = function(){
+        alert("it works!");
+    };
+});
+app.controller('UpdateController',function($scope,$routeParams,$firebase,$location){
+    //console.log($routeParams.key);
+    var usersRef = new Firebase("https://luminous-fire-4025.firebaseio.com/user/"+$routeParams.key);
+    $scope.user = $firebase(usersRef);
+    //console.log($scope.user);
+    $scope.update = function(){
+        $scope.user.$save();
+        $location.path("list");
+    }
+});
+
+app.controller('SelectController',function($scope){
+    $scope.items = [
+        "The first choice!",
+        "And another choice for you.",
+        "but wait! A third!"
+    ];
+});
